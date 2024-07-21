d4079d2bb577c2aaea03fc4b15d63bc325fefd7c
Gokul Kannan <gokulrgk1110@gmail.com>
2024-07-21 16:55:37 +0530

    Added EntityFrameworkCore and worked on Retrieving and Showing up data in View.

---
+++ GkWebApp/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using GkWebApp.Data;
+using GkWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GkWebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext Context)
+        {
+            _context = Context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categoriesList = _context.Categories.ToList();
+            return View(categoriesList);
+        }
+    }
+}
