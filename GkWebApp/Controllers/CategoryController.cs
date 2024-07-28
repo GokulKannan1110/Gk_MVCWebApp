4a90f7db710e69e908f6cb033766f8e263a57ee4
Gokul Kannan <gokulrgk1110@gmail.com>
2024-07-28 21:30:07 +0530

    Created Form to create category. Used MVC tag helpers and validations.

--- GkWebApp/Controllers/CategoryController.cs
+++ GkWebApp/Controllers/CategoryController.cs
@@ -17,5 +17,32 @@ namespace GkWebApp.Controllers
             List<Category> categoriesList = _context.Categories.ToList();
             return View(categoriesList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if(category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Name and Display Order Cannot be the same.");
+            }
+            if (category.Name != null && category.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is a invalid name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View();
+            
+        }
     }
 }
